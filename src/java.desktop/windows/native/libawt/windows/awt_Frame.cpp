JetBrains/jdk8u_jdk

JRE-938 [windows] Frame.setMaximizedBounds not hidpi-aware

src/windows/native/sun/windows/awt_Frame.cpp
@@ -1511,6 +1511,10 @@ void AwtFrame::_SetMaximizedBounds(void *param)
     if (::IsWindow(f->GetHWnd()))
     {
         DASSERT(!::IsBadReadPtr(f, sizeof(AwtFrame)));
+        x = f->ScaleUpDX(x);
+        y = f->ScaleUpDY(y);
+        width = f->ScaleUpX(width);
+        height = f->ScaleUpY(height);
         f->SetMaximizedBounds(x, y, width, height);
     }
 ret:

seanpm2001/JetBrainsRuntime

(cherry picked from commit cc97899923320e1fa17f5e44975c4a0f0ba51014)
(cherry picked from commit ccfe65be7f5e860a383318984fe58d22bb062b44)

src/java.desktop/windows/native/libawt/windows/awt_Frame.cpp
@@ -1475,6 +1475,10 @@ void AwtFrame::_SetMaximizedBounds(void *param)
     if (::IsWindow(f->GetHWnd()))
     {
         DASSERT(!::IsBadReadPtr(f, sizeof(AwtFrame)));
+        x = f->ScaleUpAbsX(x);
+        y = f->ScaleUpAbsY(y);
+        width = f->ScaleUpX(width);
+        height = f->ScaleUpY(height);
         f->SetMaximizedBounds(x, y, width, height);
     }
 ret: